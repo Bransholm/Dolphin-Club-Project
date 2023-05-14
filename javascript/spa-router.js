b50e54f1821eb912ec39ef8f867809e9fa503349
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-05-14 11:57:47 +0200

    attempt to route

--- javascript/spa-router.js
+++ javascript/spa-router.js
@@ -1,33 +1,40 @@
-
 function singlePageRouter() {
-window.addEventListener("haschange", togglePageView);
-togglePageView()
+  window.addEventListener("haschange", togglePageView);
+  togglePageView();
 }
 
-function togglePageView(){
-let hashLink = "#velkommen"; 
+function togglePageView() {
+  let hashLink = "#velkommen";
 
-if(location.hash){
-    hashLink = location.hash
+  if (location.hash) {
+    hashLink = location.hash;
+  }
+
+  hideAllViews();
 }
 
-hideAllViews()
-
-}
-
-document.querySelector("#hashLink").classList.add("unblock")
+document.querySelector("#hashLink").classList.add("active");
 setActiveLink(hashLink);
 
-function setActiveLink(view){
-    // const link = document.querySelector(...)
-    if(link){
-        link.classList.add("unblock")
-    }
+function setActiveLink(view) {
+  // const link = document.querySelector(...)
+  if (link) {
+    link.classList.add("active");
+  }
 }
 
-function hideAllViews(){
+function hideAllViews() {
+  document.querySelector().forEach((link) => link.classList.remove("active"));
 
+  document.querySelector().forEach((link) => link.classList.remove("active"));
+
+  // remove .active for all .view-content elements (all views) and .view-link elements (all links)
+  document
+    .querySelectorAll(".view-content")
+    .forEach((link) => link.classList.remove("active"));
+  document
+    .querySelectorAll(".view-link")
+    .forEach((link) => link.classList.remove("active"));
 }
 
-
-export{singlePageRouter}
+export { singlePageRouter };
