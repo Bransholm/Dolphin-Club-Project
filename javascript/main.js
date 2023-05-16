38bbc103263b3af85e9cca2e03a2f4b1656b18fa
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-05-16 13:35:18 +0200

    SPA som endelig virker

--- javascript/main.js
+++ javascript/main.js
@@ -12,15 +12,3 @@ function start() {
   singlePageRouter();
   showPreformanceForm();
 }
-
-function showSection(sectionId) {
-  // Hide all sections
-  var sections = document.getElementsByClassName("show-hide-section");
-  for (var i = 0; i < sections.length; i++) {
-    sections[i].style.display = "none";
-  }
-
-  // Show the selected section
-  var section = document.getElementById(sectionId);
-  section.style.display = "block";
-}
