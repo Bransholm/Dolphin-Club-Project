a9471d5101e69ddb8126863a519821e9fd1ce87f
Bransholm <kenneth@bransholm.dk>
2023-05-16 02:45:34 +0200

    Show and hide sections

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,15 @@
-"use strict"
+"use strict";
 
-console.log("JavaScript is running");
+console.log("Javascript is running");
 
+function showSection(sectionId) {
+  // Hide all sections
+  var sections = document.getElementsByClassName("show-hide-section");
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].style.display = "none";
+  }
+
+  // Show the selected section
+  var section = document.getElementById(sectionId);
+  section.style.display = "block";
+}
