d30f3743de021ec66bb69b07c409ec41471708e4
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-05-16 12:46:33 +0200

    retrun to SPA

--- javascript/main.js
+++ javascript/main.js
@@ -2,24 +2,14 @@
 
 console.log("Javascript is running");
 
-// import { singlePageRouter } from "./spa-router.js";
+import { singlePageRouter } from "./spa-router.js";
 import { showPreformanceForm } from "./preformance-tracking.js";
 
 window.addEventListener("load", start);
 
 function start() {
-  // singlePageRouter();
+  singlePageRouter();
   showPreformanceForm();
-  addEs();
-}
-
-function addEs() {
-  document
-    .querySelector("button1")
-    .addEventListener("click", showSection("section1"));
-  document
-    .querySelector("button2")
-    .addEventListener("click", showSection("section2"));
 }
 
 function showSection(sectionId) {
