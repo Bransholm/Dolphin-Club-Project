61e2693b739f4975aca8a93d4948334d1a9232dc
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-05-19 16:41:24 +0200

    updates

--- javascript/main.js
+++ javascript/main.js
@@ -3,16 +3,16 @@
 console.log("Javascript is running");
 
 import { singlePageRouter } from "./spa-router.js";
-import { showPerformanceForm} from "./performance-tracking.js";
+import { showPerformanceForm } from "./performance-tracking.js";
 // import { showSection } from "./router.js";
 // import { updateMemberGrid } from "./getMembers.js";
 import { startIndmelding } from "./formand.js";
-import { showMemberPerformances} from "./performance-overview.js";
+import { showMemberPerformances } from "./performance-overview.js";
 window.addEventListener("load", start);
 
 function start() {
   singlePageRouter();
   showPerformanceForm();
   showMemberPerformances();
-  // startIndmelding();
+  startIndmelding();
 }
